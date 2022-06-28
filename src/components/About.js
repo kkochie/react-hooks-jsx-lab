436dd8bd16c4615a64fd8855dd7ace80f2051f90
Kristine Kochie <kristinekochie@gmail.com>
2022-06-28 16:59:57 -0400

    commit tree 96e93b8ec24948b8f94c406523f9509eb1c7734c

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>I have two cats. They mainly sleep with an afternoon wrestling session
+      during office hours.
+    </p>
+    <img src={ image } alt="I made this" />
+  </div>;
 }
 
 export default About;
